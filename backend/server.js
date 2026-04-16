8759992f82733ba11528a0a7c0fe3e78a2a6eb78
Muhammad Haroon chaudhary <117911242+Muhammad-Haroon-Ashfaq@users.noreply.github.com>
2026-04-16 17:29:15 +0500

    Initial backend setup with auth

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+
+dotenv.config();
+
+const app = express();
+
+// Connect DB
+connectDB();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+
+
+// test route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
